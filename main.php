b5632b680cbc14d191e71d66061ed0ea0a2190a6
NoNoobSaibot <157010652+NoNoobSaibot@users.noreply.github.com>
2024-02-15 20:45:48 +1000

    Update main.php

--- main.php
+++ main.php
@@ -1,1 +1,23 @@
-<?phpsession_start();if (!isset($_SESSION['LOGIN']) && !isset($_SESSION['PASSWORD'])) :	header("HTTP/1.1 403 Access denied");	die();    else:?><html>	<head>		<meta charset="utf-8">		<title>Главная страница</title>	</head>	<body>		Добро пожаловать на сайт!<br>		<a href="outaccount.php">выйти из учетной записи</a>	</body></html><?phpendif;?>
+<?php
+session_start();
+
+if (!isset($_SESSION['LOGIN']) && !isset($_SESSION['PASSWORD'])) :
+	header("HTTP/1.1 403 Access denied");
+	die();
+    else:
+?>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Главная страница</title>
+	</head>
+	<body>
+		Добро пожаловать на сайт!<br>
+		<a href="outaccount.php">выйти из учетной записи</a>
+	</body>
+</html>
+
+<?php
+endif;
+?>
